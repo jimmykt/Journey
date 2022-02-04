9116e066dead18c442ed6dbc2b36e615c33e09e1
Ka-Kit Jimmy Tam <90652743+jimmykt@users.noreply.github.com>
2022-02-04 16:45:18 -0500

    styles with material-ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,27 @@
 import React from "react";
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
 import memories from "./assets/images/memories.png";
+import useStyles from "./styles";
+
 const App = () => {
+  const classes = useStyles();
+
   return (
-    <Container maxwidth="lg">
-      <AppBar position="static" color="inherit">
-        <Typography variant="h2" align="center">
+    <Container maxidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
           Jornney
         </Typography>
-        <img src={memories} alt="photoframe" height="60" />
+        <img
+          className={classes.image}
+          src={memories}
+          alt="photoframe"
+          height="60"
+        />
       </AppBar>
+
       <Grow in>
         <Container>
           <Grid
